8fd229dbc3e1f238cef4f76cf5b1b829e4114d92
Nxzenai <nxzenai@example.com>
2026-08-11 16:51:50 +0530

    changes

--- app/main.py
+++ app/main.py
@@ -29,10 +29,10 @@ app = FastAPI(
 # ---------------------------------------------------------
 
 origins = [
-    "http://localhost:3001",
+    #"http://localhost:3001",
     #"http://127.0.0.1:8001",
-    #"https://www.nxzenai.com",
-    "https://coral-app-8t2db.ondigitalocean.app"
+    "https://www.nxzenai.com",
+    #"https://coral-app-8t2db.ondigitalocean.app"
     
     
 
