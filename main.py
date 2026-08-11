045800401acd4d571ac0bd3b1b193a4022447092
Nxzenai <nxzenai@example.com>
2026-08-11 15:29:42 +0530

    changes

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:3001",
+        #"http://localhost:3000",
+        #"http://localhost:3001",
         #"http://127.0.0.1:3000",
         #"http://127.0.0.1:3001",
         #"https://www.nxzenai.com",
