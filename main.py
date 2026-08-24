b9550900701ae2ee1d50752b074eeca48c17cc50
Bharghav <bhargav@nxzenai.com>
2026-08-24 17:32:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        #"https://www.nxzenai.com",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:3001"
+        "https://www.nxzenai.com",
+        #"http://localhost:3000",
+        #"http://127.0.0.1:3000",
+        #"http://localhost:3001"
     ],
     allow_credentials=True,
     allow_methods=["*"],
